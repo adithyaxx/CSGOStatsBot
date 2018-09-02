81d2bd4718a9134a5b0e565c5799c8a6a2dfc1d8
Prem Adithya <premadithya@ymail.com>
2018-09-02 20:28:52 +0800

    Added Heroku Config Vars

--- src/BotConfig.java
+++ src/BotConfig.java
@@ -1,4 +1,4 @@
 public class BotConfig {
-    public static final String BOT_TOKEN = "<token>";
-    public static final String API_KEY = "<api_key>";
+    public static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+    public static final String API_KEY = System.getenv("API_KEY");
 }
